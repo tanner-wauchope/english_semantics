0e59302c73e630ae535704297b25ce5003ea6360
Tanner Wauchope <tannershanew@gmail.com>
2015-05-02 17:00:01 -0700

    I modified the scanner so that clitics are classified as their own lexemes. This will allow the semantic module to distinguish between noun strings and possessives. It also will allow the possessive noun to receive have complements.

--- language/syntax/stages/scan.py
+++ language/syntax/stages/scan.py
@@ -1,44 +1,43 @@
-from language.syntax.word_classes import quote
 
 
-def quote_split(text):
+def phrase_split(phrase):
     """
-    :param text: text with an even number of quotation marks
-    :return: sequence of texts with the quoted sections separated
+    :param phrase: text lacking quotes but possibly having possessives
+    :return: a list of words and possibly clitics
     """
-    lexemes = ['']
-    for character in text:
-        opened_quote = lexemes[-1].startswith('"')
-        if character != ' ' or opened_quote:
-            lexemes[-1] += character
-        if character == ' ' and not opened_quote or quote.match(lexemes[-1]):
-            lexemes.append('')
+    lexemes = []
+    for lexeme in phrase.split(' '):
+        try:
+            noun, clitic = lexeme.split("'")
+            lexemes.append(noun)
+            lexemes.append("'" + clitic)
+        except ValueError:
+            lexemes.append(lexeme)
     return lexemes
 
 
-def punctuation_split(text, delimiter):
+def lexemes(line):
     """
-    :param text: text to be split into segments
-    :param delimiter: text that delimits each segment
-    :return: non-whitespace segments
+    :param text: text that may contain possessives or embedded quotes
+    :return: a list of lexemes, which are words, clitics, or quotes
     """
-    result = []
-    for segment in text.split(delimiter):
-        if not segment.isspace():
-            result.append(segment)
-    return result
+    lexemes = []
+    for i, segment in enumerate(line.split('"')):
+        if i % 2 == 0:
+            lexemes.extend(phrase_split(segment))
+        else:
+            lexemes.append('"' + segment + '"')
+    return lexemes
 
 
-def scan(block):
+def scan(paragraph):
     """
-    :param block: a text paragraph
+    :param paragraph: a text whose lines do not have trailing whitespace
     :return: sentences that contain lines that contain lexemes
     """
     sentences = []
-    for sentence in punctuation_split(block, '.\n'):
+    for sentence in paragraph.split('.\n')[:-1]:
         sentences.append([])
-        for line in punctuation_split(sentence, ',\n\t'):
-            sentences[-1].append([])
-            for lexeme in quote_split(line):
-                sentences[-1][-1].append(lexeme)
+        for line in sentence.split(',\n\t'):
+            sentences[-1].append(lexemes(line))
     return sentences
