1a864e6cf1b0e8ace823df666f5ac83bd3974b5f
Tanner Wauchope <tannershanew@gmail.com>
2015-12-30 00:14:01 -0800

    I wrote automated tests for the module register.py. In the process, I made a couple design decisions. Noun phrases are now passed by reference (new containers are not created when checking compatibility). I added several magic methods to the Noun class. I made the "verb" helper function be a function that takes a signature a returns a decorator so that the signature does not have to be crammed into function annotations.

--- python/register.py
+++ python/register.py
@@ -6,50 +6,37 @@ def register() -> tuple:
     """
     nouns = {}
 
-    def noun(name) -> Noun:
+    def noun(name=None) -> Noun:
         """
         :param str name: the name of the noun to get or create
         :return: the noun that has the name or a new noun
         """
-        nouns[name] = nouns.get(name) or type(name, (Noun,), {})
-        return nouns[name]()
+        if name is None:
+            return Noun()
+        entry = nouns.get(name) or type(name, (Noun,), {})
+        nouns[name] = entry
+        return entry()
 
     verbs = {}
 
-    def verb(function) -> Verb:
+    def verb(*kinds):
         """
-        :param callable function: a new definition for a verb
-        :return: the verb whose name is the function's name or a new verb
+        :param kinds: the signature for definitions the returned function adds
+        :return: a function that adds new definitions to verbs
         """
-        name = function.__name__
-        verbs[name] = verbs.get(name) or Verb()
-        verbs[name].functions.append(function)
-        return verb
-
+        def define(function) -> Verb:
+            """
+            :param callable function: a new definition for a verb
+            :return: the verb whose name is the function's name or a new verb
+            """
+            function.kinds = kinds
+            name = function.__name__
+            entry = verbs.get(name) or Verb()
+            entry.functions.append(function)
+            verbs[name] = entry
+            return entry
+        return define
     return noun, verb
-
-
-class Noun:
-    """
-    Subclasses of this class are nouns.
-    Instances of this class are noun phrases.
-    :type properties: dict
-    :type entities: list
-    """
-    def __init__(self, properties=None, entities=None) -> None:
-        """
-        :param dict properties: key-value pairs this noun phrase satisfies
-        :param list entities: constituents of this noun phrase
-        """
-        self.properties = properties or {}
-        self.entities = entities or []
-
-    def __getitem__(self, item) -> 'Noun':
-        """
-        :param int|slice item: the index or indexes to be returned
-        :return: a noun with the correct subset of entities
-        """
-        return Noun(self.properties, self.entities[item])
 
 
 class Verb:
@@ -68,49 +55,88 @@ class Verb:
             distribute(function, matches(function, args))
 
 
-def matches(function, args) -> list:
-    """
-    :param function: a function to which args will be distributed
-    :param args: unconstrained arguments not wrapped for distribution
-    :return: constrained arguments wrapped for distribution
-    """
-    result = []
-    for key, arg in zip(function.__code__.co_varnames, args):
-        kind = function.__annotations__[key]
-        entities = compatible(kind, arg.entities)
-        if kind.size == 1:
-            result.append(entities)
-        else:
-            result.append([entities])
-    return result
-
-
-def compatible(kind, entities) -> list:
-    """
-    :param Noun kind: a noun phrase whose properties constrain the entities
-    :param list entities: the entities that might be compatible with the kind
-    :return: the entities that compatible with the kind
-    """
-    result = []
-    for entity in entities:
-        for key, value in kind.properties.items():
-            if entity.properties[key] == value:
-                result.append(entity)
-    return result
-
-
 def distribute(function, args, i=0) -> None:
     """
     :param callable function: a function to be invoked, possibly multiple times
-    :param list args: constrained arguments ready wrapped for distribution
+    :param list args: constrained arguments wrapped for distribution
     :param int i: the index of the last distributed argument
     """
     if i == len(args):
-        function(*args)
-    for value in args[i]:
+        return function(*args)
+    arg = args[i]
+    if arg is None or isinstance(arg, Noun) and not arg.entities:
+        arg = [arg]
+    for value in arg:
         arguments = list(args)
         arguments[i] = value
         distribute(function, arguments, i=i + 1)
+
+
+def matches(function, args):
+    """
+    :param callable function: a function to which args will be distributed
+    :param tuple args: unconstrained arguments not wrapped for distribution
+    :return: constrained arguments wrapped for distribution
+    """
+    result = []
+    for i, arg in enumerate(args):
+        kind = function.kinds[i]
+        constrained_arg = compatible(kind, arg)
+        if kind.entities:
+            result.append([constrained_arg])
+        else:
+            result.append(constrained_arg)
+    return result
+
+
+def compatible(kind, arg) -> 'Noun':
+    """
+    :param Noun kind: a noun phrase whose properties constrain the entities
+    :param Noun arg: the entities that might be compatible with the kind
+    :return: the entities that are compatible with the kind
+    """
+    entities = arg.entities or [arg]
+    for entity in entities:
+        for key, value in kind.properties.items():
+            if entity.properties[key] != value:
+                return None
+    return arg
+
+
+class Noun:
+    """
+    Subclasses of this class are nouns.
+    Instances of this class are noun phrases.
+    :type properties: dict
+    :type entities: list
+    """
+    def __init__(self, properties=None, entities=None) -> None:
+        """
+        :param dict properties: key-value pairs this noun phrase satisfies
+        :param list entities: constituents of this noun phrase
+        """
+        self.properties = properties or {}
+        self.entities = entities or []
+
+    def __eq__(self, other) -> bool:
+        """
+        :param other: any object
+        :return: whether self and other have same class and member variables
+        """
+        return hasattr(other, '__dict__') and self.__dict__ == other.__dict__
+
+    def __iter__(self):
+        """
+        :return: the constituent entities of the noun phrase
+        """
+        return iter(self.entities)
+
+    def __getitem__(self, item) -> 'Noun':
+        """
+        :param int|slice item: the index or indexes to be returned
+        :return: a noun with the correct subset of entities
+        """
+        return Noun(self.properties, self.entities[item])
 
 
 # # option 1
